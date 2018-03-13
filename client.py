2f904cbe2db702b1d8bee6a051435b78eb10e762
zhmi1204 <zhmi1204@163.com>
2018-03-13 09:34:53 +0800

    no
    no

--- client.py
+++ client.py
@@ -25,7 +25,8 @@
 #python2
 import socket
 
-address = ('47.95.233.40',180)
+address = ('47.95.233.40',11234)
+# address = ('172.16.252.50',180)
 # address = ('106.39.0.94',180)
 # address = ('127.0.0.1',10372)
 udpClientSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
