8c2bd74279692eb342ad9af01c2755b96f080fb3
zhmi1204 <zhmi1204@163.com>
2018-03-13 19:18:37 +0800

    no
    no

--- server.py
+++ server.py
@@ -27,12 +27,12 @@ from time import ctime
 
 buffer=2048
 address=('127.0.0.1',8888)
-udpsock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udpServerSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 # udpsock.listen(5)
-udpsock.bind(address)
+udpServerSock.bind(address)
 while True:
     print 'waiting for message...'
-    data,addr=udpsock.recvfrom(buffer)
-    udpsock.sendto('[%s]%s' %(ctime(),data),addr)
+    data,addr=udpServerSock.recvfrom(buffer)
+    udpServerSock.sendto('[%s]%s' %(ctime(),data),addr)
     print 'received data '+ data +' from and retuned ',addr
-udpsock.close()
+udpServerSock.close()
