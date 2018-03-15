beb7ff292db0f1aa86a3898202a86c6fd2cccf53
zhmi1204 <zhmi1204@163.com>
2018-03-15 10:57:37 +0800

    test
    test

--- client.py
+++ client.py
@@ -1,34 +1,13 @@
 #coding:utf8
-# python3
-# from socket import *
-#
-# # PC端公网IP为106.39.0.94
-# host = '47.95.233.40'
-# port =12345
-# bufsize = 1024
-#
-# addr = (host,port)
-# udpClient = socket(AF_INET,SOCK_DGRAM)
-#
-# while True:
-#     data = input('>>>')
-#     # if not data:
-#     #     break
-#     data = data.encode(encoding="utf-8")
-#     udpClient.sendto(data,addr)
-#     data.addr = udpClient.recvfrom(bufsize)
-#     print(data.decode(encoding="utf-8"),'from',addr)
-#     # print(data,addr)
-#
-# udpClient.clos
 
 #python2
 import socket
 
-address = ('47.95.233.40',31204)
+# address = ('47.104.188.27',31204)
+# address = ('47.95.233.40',31204)
 # address = ('172.16.252.50',180)
 # address = ('106.39.0.94',180)
-# address = ('127.0.0.1',10372)
+address = ('127.0.0.1',50000)
 udpClientSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 # udpClientSocket.bind(address)
 while True:
@@ -37,5 +16,5 @@ while True:
         break
     udpClientSocket.sendto(data,address)
     data,address = udpClientSocket.recvfrom(2048)
-    print 'received data '+ data +' from and retuned to addr ',address
+    print 'received '+ data +' from ',address
 udpClientSocket.close()
--- server.py
+++ server.py
@@ -1,38 +1,26 @@
-# python3
-from socket import *
-from time import ctime
-
-# host = ''
-# port = 12345
-# bufsize = 1024
-# addr = (host,port)
-#
-# udpServer = socket(AF_INET,SOCK_DGRAM)
-# udpServer.bind(addr)
-#
-# while True:
-#     print("waiting for connecting...")
-#     data,addr = udpServer.recvfrom(bufsize)
-#     data = data.decode(encoding='utf-8').upper()
-#     data = "at %s : %s "%(ctime(),data)
-#     udpServer.sendto(data.encode('utf-8'),addr)
-#     print("received from and return to :",addr)
-#
-# udpServer.close()
-
-# dwhadwadwad
 # python2
 import socket
 from time import ctime
+import time
+import threading
+import sys
 
-buffer=2048
-address=('127.0.0.1',31204)
+address=('127.0.0.1',50000)
 udpServerSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# udpsock.listen(5)
 udpServerSock.bind(address)
+
+# def thread_wait():
+#     thread = threading.Thread
+#
+# def thread_print():b
+#     pass
+
 while True:
-    print 'waiting for message...'
-    data,addr=udpServerSock.recvfrom(buffer)
-    udpServerSock.sendto('[%s]%s' %(ctime(),data),addr)
-    print 'received data '+ data +' from and retuned ',addr
+    print 'waiting for message'
+    data,addr=udpServerSock.recvfrom(2018)
+    if not data:
+        break
+    # udpServerSock.sendto('[%s]%s' %(ctime(),data),addr)
+    udpServerSock.sendto('Helloworld' ,addr)
+    print 'received '+ data +' from ',addr
 udpServerSock.close()
