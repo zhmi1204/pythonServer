c3b19aa5f2a1c98b3ad97dbfbf20d38dd06157c9
zhmi1204 <zhmi1204@163.com>
2018-03-17 15:20:51 +0800

    test
    test

--- server.py
+++ server.py
@@ -1,19 +1,9 @@
 # python2
 import socket
-from time import ctime
-import time
-import threading
-import sys
-#3471/udp
+
 address=('127.0.0.1',3471)
 udpServerSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 udpServerSock.bind(address)
-
-# def thread_wait():
-#     thread = threading.Thread
-#
-# def thread_print():b
-#     pass
 
 while True:
     print 'waiting for message'
@@ -21,7 +11,6 @@ while True:
     print("Got message!waiting for re-send!")
     if not data:
         break
-    # udpServerSock.sendto('[%s]%s' %(ctime(),data),addr)
     udpServerSock.sendto('Helloworld' ,addr)
     print 'received '+ data +' from ',addr
 udpServerSock.close()
