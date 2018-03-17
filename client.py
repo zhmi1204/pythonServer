3089c4a1fb0bdbb9f4e4e10a86987384bb08610c
zhmi1204 <zhmi1204@163.com>
2018-03-17 14:51:01 +0800

    快好了
    烧香

--- client.py
+++ client.py
@@ -3,16 +3,9 @@
 #python2
 import socket
 
-# address = ('47.104.188.27',31204)
-# address = ('47.95.233.40',31204)
-# address = ('172.16.252.50',180)
-# address = ('106.39.0.94',180)
-# address = ('127.0.0.1',50000)
-#3471/udp
-# address = ('47.95.233.40',3471)
 address = ('47.104.188.40',3471)
 udpClientSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# udpClientSocket.bind(address)
+
 while True:
     data = raw_input("input >>> ")
     if not data:
