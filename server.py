305eb5df9ef444e4a36097c4b83de73b58f6c4e3
zhmi1204 <zhmi1204@163.com>
2018-03-26 00:36:56 +0800

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)

--- server.py
+++ server.py
@@ -1,20 +1,37 @@
 #coding:utf8
-# python2 server
+# python server
 import socket
+import pymysql
+
+def saveInf(data):
+	conn = pymysql.connect(host='localhost',user='root',password='root',charset='utf8',port=3306,database='pythonServer')
+	cur = conn.cursor()
+	sql = "insert into received (text) values " + (str(data))
+	cur.execute(sql)
+	conn.commit()
+	cur.close()
+	conn.close()
 
 address=('0.0.0.0',3471)
 udpServerSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 udpServerSock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 udpServerSock.bind(address)
+conn = pymysql.connect(host='localhost',user='root',password='root',charset='utf8',port=3306,database='pythonServer')
+cur = conn.cursor()
 while True:
-    print('waiting for message')
-    data,addr=udpServerSock.recvfrom(2048)
-    data = data.decode('utf-8')
-    print('Got message!')
-    if not data:
-        break
-    string=str(data) + "--- from---  " +str(addr)
-    print('received '+ data +' from ',addr)
-    data = data.encode('utf-8')
-    udpServerSock.sendto(data,addr)
+	print('waiting for message')
+	data,addr=udpServerSock.recvfrom(2048)
+	data = data.decode('utf-8')
+	sql = "insert into received(text) values(%s)"
+	cur.execute(sql,data)
+	conn.commit()
+	print('Got message!')
+	if not data:
+		break
+	string=str(data) + "--- from---  " +str(addr)
+	print('received '+ data +' from ',addr)
+	data = data.encode('utf-8')
+	udpServerSock.sendto(data,addr)
+cur.close()
+conn.close()
 udpServerSock.close()
